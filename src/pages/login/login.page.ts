fba15505091dd07d635ffce51ebeebcebe5ec89a
Miguel Gonzalez Villa <miguel.gonzalez.villa@gmail.com>
2017-10-17 16:17:36 +0100

    Clase Login para acceso de clientes

--- src/pages/login/login.page.ts
+++ src/pages/login/login.page.ts
@@ -1,14 +1,76 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, MenuController, AlertController, LoadingController } from 'ionic-angular';
+import { FormBuilder } from '@angular/forms';
+
+import { HomePage } from "../pages";
+
+import { AngularFire } from "angularfire2";
+
+import _ from 'lodash';
 
 @Component({
     templateUrl: 'login.page.html',
     selector: 'login.page.scss'
 })
+
 export class LoginPage {
 
-    constructor(public navCtrl: NavController) {
+    loginForm: any;
+    usersData: any;
+    
+    constructor(public navCtrl: NavController, 
+                private menuController: MenuController,
+                private alertCtrl: AlertController,
+                private loadingCtrl: LoadingController,
+                private formBuilder: FormBuilder,
+                private angularFire: AngularFire) {
 
+        this.menuController.enable(false);
+
+        this.loginForm = formBuilder.group({
+            dni: ['']
+        });
+    }
+
+    loginUser(){
+
+        this.angularFire.database.list('/pedidos').subscribe(data => {
+            this.usersData = _.chain(data)
+                            .filter(a => a.dni === this.loginForm.value.dni)
+                            .value();
+                            
+            if(this.loginForm.value.dni.length < 9){
+                let alert = this.alertCtrl.create({
+                    message: "El DNI no está escrito correctamente.",
+                    buttons: [
+                        {
+                            text: "Ok",
+                            role: 'cancel'
+                        }
+                    ]
+                });
+                alert.present();
+
+            } else if (this.usersData.length == 0) {
+                console.log("Ningun registro");                
+                let alert = this.alertCtrl.create({
+                    message: "El DNI solicitado puede no tener pedidos encargados.",
+                    buttons: [
+                        {
+                            text: "Ok",
+                            role: 'cancel'
+                        }
+                    ]
+                });
+                alert.present();
+            } else {
+                console.log(this.loginForm.value.dni);
+                this.navCtrl.setRoot(HomePage, this.loginForm.value.dni);
+            }
+
+        });
+
+        
     }
 
 }

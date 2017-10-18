0b5bc0dfffa1797a0a78504d00d653997cf1032f
Miguel Gonzalez Villa <miguel.gonzalez.villa@gmail.com>
2017-10-18 11:51:17 +0100

    Estructura principal y métodos de control de la clase Order

--- src/pages/order/order.page.ts
+++ src/pages/order/order.page.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, NavParams, AlertController, ToastController } from 'ionic-angular';
+
+import { AngularFire, FirebaseListObservable } from "angularfire2";
+
+import { MapPage } from "../pages";
 
 @Component({
     templateUrl: 'order.page.html',
@@ -7,8 +11,58 @@ import { NavController } from 'ionic-angular';
 })
 export class OrderPage {
 
-    constructor(public navCtrl: NavController) {
+    orderData: any;
+    orderObs: FirebaseListObservable<any>;
 
+    constructor(public nav: NavController, 
+                private navParams: NavParams,
+                private alertController: AlertController,
+                private angularFire: AngularFire,
+                private toastController: ToastController) {
+
+        
+        this.orderData = this.navParams.data;
+
+    }
+
+    ionViewDidLoad(){
+        this.orderObs = this.angularFire.database.list('/pedidos');
+    }
+
+    goToMap(){
+        this.nav.push(MapPage);
+    }
+
+    addName(){
+        let prompt = this.alertController.create({
+            title: 'Nuevo nombre',
+            message: "¿Desea introducir un nombre para el pedido seleccionado?",
+            inputs: [{
+                type: 'text',
+                name: 'nombre'
+            }],
+            buttons: [
+                {
+                    text: 'Cancelar'
+                },
+                {
+                    text: 'Añadir',
+                    handler: data => {
+                        this.orderObs.update(this.orderData.$key, {nombre: data.nombre});
+
+                        let toast = this.toastController.create({
+                            message: "Se ha registrado el nombre",
+                            duration: 4000,
+                            position: 'bottom'
+                        });
+
+                        toast.present();
+                    }
+                }
+            ]
+
+        });
+        prompt.present();
     }
 
 }

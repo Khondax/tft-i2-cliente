6a8df2a37e1bdcea452c8d116d48a72e7ab1285e
Miguel Gonzalez Villa <miguel.gonzalez.villa@gmail.com>
2017-10-19 14:09:24 +0100

    Añadida página de historial con buscador de pedidos

--- src/pages/registry/registry.page.ts
+++ src/pages/registry/registry.page.ts
@@ -1,14 +1,76 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, LoadingController, AlertController } from 'ionic-angular';
 
-@Component({
-    templateUrl: 'registry.page.html',
-    selector: 'registry.page.scss'
-})
-export class RegistryPage {
+import { AuthData } from '../../providers/authdata';
 
-    constructor(public navCtrl: NavController) {
+import { AngularFire } from "angularfire2";
+
+import _ from 'lodash';
+
+ @Component ({
+     templateUrl: 'registry.page.html',
+     selector: 'registry.page.scss'
+ })
+
+ export class RegistryPage {
+
+    orders = [];
+    queryText: string = "";
+    allData: any;
+
+    constructor(public nav: NavController,
+                public alertController: AlertController,
+                public angularFire: AngularFire,
+                public loadingController: LoadingController,
+                private authData: AuthData){
 
     }
 
-}
+    ionViewDidLoad(){
+
+        let loader = this.loadingController.create ({
+            content: 'Obteniendo datos...',
+            spinner: 'bubbles'
+        });
+
+        loader.present().then(() => {
+
+            this.angularFire.database.list('/pedidos').subscribe(data => {
+                this.allData = _.chain(data)
+                               .filter(a => a.dni === this.authData.getCurrentDni())
+                               .orderBy('fechaEntrega', 'desc')
+                               .groupBy(fecha => fecha.fechaEntrega.split('T)').shift())
+                               .toPairs()
+                               .map(item => _.zipObject(['date', 'order'], item))
+                               .value();
+
+                this.orders = _.chain(this.allData)
+                              .orderBy('date', 'desc')
+                              .value();
+
+                loader.dismiss();
+
+            });
+        });
+
+    }
+
+    search(){
+        let queryTextLower = this.queryText.toLowerCase();
+        let filteredOrders = [];
+        
+        _.forEach(this.allData, dat => {
+            let orders = _.filter(dat.order, or => (<any>or).repartidor.toLowerCase()
+            .includes(queryTextLower) || (<any>or).fechaEntrega.toLowerCase()
+            .includes(queryTextLower) || (<any>or).remitente.toLowerCase()
+            .includes(queryTextLower) || (<any>or).idPaquete.toString().includes(queryTextLower));
+            if (orders.length) {
+                filteredOrders.push({ date: dat.date, order: orders});
+            }
+        });
+
+        this.orders = filteredOrders;
+    }
+
+
+ }
